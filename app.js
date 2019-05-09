ea7683eb0084786d873acc7eefc6d1a5641b1cf3
Alan lau <41530612+lauman64@users.noreply.github.com>
2019-05-09 16:30:22 +1200

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -188,6 +188,12 @@ app.get("/favicon.ico", function(req, res){
     res.sendStatus(204);
 });
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
